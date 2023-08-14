5b8d54f0332fcaabea0100e9746f76b44a023a58
cak-17 <fracatjam@gmail.com>
2023-08-14 19:18:56 +0200

    2 tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from api.models import Product
-
+from api.serializers import ProductSerializer
 
 class ProductTestCase(TestCase):
   def testProduct(self):
@@ -12,3 +12,14 @@ class ProductTestCase(TestCase):
     self.assertEqual(product.name, "Test1")
     self.assertEqual(product.description, "This is a test")
     self.assertEqual(product.price, 5.0)
+
+
+class ProductSerializerTestCase(TestCase):
+ def testProductSerializer(self):
+   data =  {
+      "name": "Pizza2",
+      "description": "A testy pizza",
+      "price": 42.0
+   }
+   product_serializer = ProductSerializer(data=data, partial=True)
+   self.assertEqual(product_serializer.is_valid(), True)
