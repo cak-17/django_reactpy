4e0ea267c0da958e2e61f0279d80344307347400
cak-17 <fracatjam@gmail.com>
2023-08-14 18:53:13 +0200

    second commit

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from api.models import Product
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):
+  def testProduct(self):
+    product = Product(
+      name="Test1",
+      description="This is a test",
+      price=5.0
+    )
+    self.assertEqual(product.name, "Test1")
+    self.assertEqual(product.description, "This is a test")
+    self.assertEqual(product.price, 5.0)
