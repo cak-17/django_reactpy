76cded79541f782b493ac876be4b3b7f42d4516b
cak-17 <98436543+cak-17@users.noreply.github.com>
2023-08-14 18:44:23 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9prud=s@#q2z_ldm1q)as+xbjrvo2+jarkd87e*__+=u+b00yr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fracat-think.local", "localhost", "*"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
